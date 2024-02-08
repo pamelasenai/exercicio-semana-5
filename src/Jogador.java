bee229541ebd0948a22d2d4a96e43e038541a6ab
Pâmela Vieira da Silva <57575373+panhavsilva@users.noreply.github.com>
2024-02-07 22:06:32 -0300

    Exercício 2

--- src/Jogador.java
+++ src/Jogador.java
@@ -4,6 +4,13 @@ public class Jogador {
     private int pontuacao;
     private int numeroTentativas;
 
+    public Jogador(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.pontuacao = 0;
+        this.numeroTentativas = 0;
+    }
+
     public void adicionaPontos(int pontos){
         pontuacao += pontos;
         System.out.println("O jogador " + nome + " ganhou mais " + pontos + " pontos.");
@@ -20,5 +27,37 @@ public class Jogador {
 
     public void adicionaTentativa(){
         numeroTentativas++;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setPontuacao(int pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+
+    public int getNumeroTentativas() {
+        return numeroTentativas;
+    }
+
+    public void setNumeroTentativas(int numeroTentativas) {
+        this.numeroTentativas = numeroTentativas;
     }
 }
